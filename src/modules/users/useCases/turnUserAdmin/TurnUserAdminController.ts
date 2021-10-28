176a1a1fe720cff4170115efef0d38db14f9cf31
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-10-28 17:04:38 -0300

    changed schema return user

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -11,7 +11,7 @@ class TurnUserAdminController {
     try {
       const user = this.turnUserAdminUseCase.execute({ user_id });
 
-      return response.status(200).json({ user });
+      return response.status(200).json(user);
     } catch (error) {
       return response.status(404).json({ error: error.message });
     }
