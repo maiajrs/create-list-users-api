cf665cfd81f8226c53a0d5f5d1f176eb2b00ed5e
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-10-28 16:48:48 -0300

    throw an error if user is not an admin

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,12 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // const userExists = this.usersRepository.findById(user_id);
-    // if (!userExists) {
-    const users = this.usersRepository.list();
-    // return userExists;
-    // }
-    return users;
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists || !userExists.admin) {
+      throw new Error("User not found or not an admin");
+    }
+    return this.usersRepository.list();
   }
 }
 
